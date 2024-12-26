b5daae286eaba743f6b545340b5e6d7b7c8e9adc
@sai133 <sai@cloudexpresssolutions.com>
2024-12-26 13:48:56 +0530

    dsp

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-  swcMinify: true, // Enable SWC minification
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        maxSize: 250000, // Limit chunk size to 250 KB
-      };
-    }
-    return config;
-  },
-});
+const nextConfig = {};
 
 export default nextConfig;
+
+
+// import withBundleAnalyzer from '@next/bundle-analyzer';
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true',
+//   swcMinify: true, // Enable SWC minification
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.optimization.splitChunks = {
+//         chunks: 'all',
+//         maxSize: 250000, // Limit chunk size to 250 KB
+//       };
+//     }
+//     return config;
+//   },
+// });
+
+// export default nextConfig;
