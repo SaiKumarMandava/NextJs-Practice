2a4c908f23f3ae93a82a61d87601818c542b58cc
@sai133 <sai@cloudexpresssolutions.com>
2024-12-26 17:50:45 +0530

    saim

--- src/app/addProducts/allProducts/allmobile/page.js
+++ src/app/addProducts/allProducts/allmobile/page.js
@@ -11,7 +11,13 @@ export default function AllMobiles() {
 
         const mobileFetch = async ()=>{
             try{
-              const response = await fetch(`/api/products/mobiles`)
+              const response = await fetch(`${process.env.NEXT_PUBLIC_URI}products/mobiles`,{
+                method: 'GET',
+                mode: 'no-cors',
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
               const data = await response.json()
               setMobileData(data.mobileData)
             }catch(e){
